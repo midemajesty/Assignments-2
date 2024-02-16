2f35b366c8535853943dbcb4e0d6f82ef8fb1d4e
midemajesty <158846551+midemajesty@users.noreply.github.com>
2024-02-16 17:46:47 +0100

    Add files via upload

---
+++ Assignments.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Solution {
+    static void Main(string[] args) {
+        int i = 4;
+        double d = 4.0;
+        string s = "Polyilaro";
+        
+        int j = 1;
+        double e = 1.0;
+        string t = "is the best place to learn";
+
+        
+        Console.WriteLine(i + j);
+        Console.WriteLine(d + e);
+        Console.WriteLine(s + " " + t);
+    }
+}
